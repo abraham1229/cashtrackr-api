a49bb151d26314ec86f863fb91cd33f027635a48
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-07-01 16:59:14 -0600

    Feat: add post and get all Budget

--- src/controllers/BudgetController.ts
+++ src/controllers/BudgetController.ts
@@ -1,11 +1,29 @@
 import type { Request, Response } from 'express'
+import Budget from '../models/Budget'
 
 export class BudgetController {
   static getAll = async (req: Request, res: Response) => {
-    console.log('Desde /api/budgets')
+    try {
+      const budgets = await Budget.findAll({
+        order: [
+          ['createdAt', 'DESC']
+        ],
+        //TODO: filter by authenticats user
+      })
+      
+      res.json(budgets)
+    } catch {
+      res.status(500).json({error: 'Unexpected error'})
+    }
   }
   static create = async (req: Request, res: Response) => {
-    console.log('post /api/budgets')
+    try {
+      const budget = new Budget(req.body)
+      await budget.save()
+      res.status(201).json('Budget created')
+    } catch {
+      res.status(500).json({error: 'Unexpected error'})
+    }
   }
   static getById = async (req: Request, res: Response) => {
     console.log('By id desde get /api/budgets')
--- src/routes/budgetRouter.ts
+++ src/routes/budgetRouter.ts
@@ -1,10 +1,18 @@
 import { Router } from "express";
+import { body } from "express-validator"
 import { BudgetController } from "../controllers/BudgetController";
+import { handleInputErrors } from "../middleware/validation";
 
-const router = Router() 
+const router = Router()
 
 router.get('/', BudgetController.getAll)
-router.post('/', BudgetController.create)
+router.post('/',
+  body('name').notEmpty().withMessage('Budget is required.'),
+  body('amount').notEmpty().withMessage('Amount is required.')
+    .isNumeric().withMessage('Amount must be a number.')
+    .custom(value => value > 0).withMessage('Amount must be greater than zero.'),
+  handleInputErrors,
+  BudgetController.create)
 router.get('/:id', BudgetController.getById)
 router.put('/:id', BudgetController.updateById)
 router.delete('/:id', BudgetController.deleteById)
