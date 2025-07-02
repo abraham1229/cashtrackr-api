4be2e1236e55b6cda2b3fb79e38e217bbbfe16fb
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-07-02 14:57:42 -0600

    Feat: Adds user model and relations

--- src/models/Budget.ts
+++ src/models/Budget.ts
@@ -1,16 +1,19 @@
-import { Table, Column, DataType, HasMany, BelongsTo, ForeignKey, Model } from 'sequelize-typescript'
+import { Table, Column, DataType, HasMany, BelongsTo, ForeignKey, Model, AllowNull } from 'sequelize-typescript'
 import Expense from './Expense'
+import User from './User'
 
 @Table({
   tableName: 'budgets'
 })
 
 class Budget extends Model {
+  @AllowNull(false)
   @Column({
     type: DataType.STRING(100)
   })
   declare name: string
 
+  @AllowNull(false)
   @Column({
     type: DataType.DECIMAL
   })
@@ -22,6 +25,11 @@ class Budget extends Model {
   })
   declare expenses: Expense[]
 
+  @ForeignKey(() => User)
+  declare userId: number
+
+  @BelongsTo(() => User)
+  declare user: User
 }
 
 export default Budget
