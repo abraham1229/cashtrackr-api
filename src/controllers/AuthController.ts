6c69c9b4f308bf35b172c72b4aecc372438653c2
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-07-03 15:51:27 -0600

    Se agrega creacion de cuenta con hash, salt y token de activacion

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,11 +1,25 @@
 import { type Request, type Response } from 'express'
 import User from '../models/User'
+import { hashPassword } from '../utils/auth'
+import { generateToken } from '../utils/token'
 
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => { 
+    const { email, password } = req.body
+    
+    // Prevent duplicates
+    const userExists = await User.findOne({where: {email: email}})
+
+    if (userExists) {
+      const error = new Error('The email is already in use')
+      return res.status(409).json({error: error.message})
+    }
+
     try {
       const user = new User(req.body)
-
+      user.password = await hashPassword(password)
+      user.token = generateToken()
+      
       await user.save()
       res.json('Account created successfully')
 
