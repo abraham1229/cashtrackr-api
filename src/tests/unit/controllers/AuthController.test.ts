c669caaba531f877e70220fb13aa6ad48487710a
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-07-07 15:50:19 -0600

    feat: end authController unit testing

--- src/tests/unit/controllers/AuthController.test.ts
+++ src/tests/unit/controllers/AuthController.test.ts
@@ -1,13 +1,15 @@
 import { createResponse, createRequest } from "node-mocks-http";
 import { AuthController } from "../../../controllers/AuthController";
 import User from "../../../models/User";
-import { hashPassword } from "../../../utils/auth";
+import { checkPassword, hashPassword } from "../../../utils/auth";
 import { generateToken } from "../../../utils/token";
 import { AuthEmail } from "../../../emails/AuthEmail";
+import { generateJWT } from "../../../utils/jwt";
 
 jest.mock("../../../models/User") //Automate mocking
 jest.mock("../../../utils/auth")
 jest.mock("../../../utils/token")
+jest.mock("../../../utils/jwt")
 
 describe('AuthController.createAccount', () => {
 
@@ -75,5 +77,120 @@ describe('AuthController.createAccount', () => {
       token: testToken
     })
     expect(res.statusCode).toBe(201)
+  })
+})
+
+describe('AuthController.login', () => { 
+
+  it('should return 404 if user is not found', async () => {
+    (User.findOne as jest.Mock).mockResolvedValue(null)
+    
+    const req = createRequest({
+      method: 'POST',
+      url: '/api/auth/login',
+      body: {
+        email: 'test@test.com',
+        password: 'testpassword'
+      }
+    })
+    const res = createResponse()
+
+    await AuthController.login(req, res)
+    
+    const data = res._getJSONData()
+    expect(res.statusCode).toBe(404)
+    expect(data).toEqual({ error: 'User not found' })
+  })
+
+  it('should return 403 if account is not confirmed', async () => {
+    (User.findOne as jest.Mock).mockResolvedValue({
+      id: 1,
+      email: 'tes@test.com',
+      password: 'password',
+      confirmed: false //Account is not confirmed
+    })
+    
+    const req = createRequest({
+      method: 'POST',
+      url: '/api/auth/login',
+      body: {
+        email: 'test@test.com',
+        password: 'testpassword'
+      }
+    })
+    const res = createResponse()
+
+    await AuthController.login(req, res)
+    
+    const data = res._getJSONData()
+    expect(res.statusCode).toBe(403)
+    expect(data).toEqual({ error: 'Account is not confirmed' })
+  })
+
+  it('should return 401 if password is incorrect', async () => {
+
+    const userMock = {
+      id: 1,
+      email: 'tes@test.com',
+      password: 'hashed_password',
+      confirmed: true
+    };
+    
+    (User.findOne as jest.Mock).mockResolvedValue(userMock)
+
+
+    const req = createRequest({
+      method: 'POST',
+      url: '/api/auth/login',
+      body: {
+        email: 'test@test.com',
+        password: 'password'
+      }
+    })
+    const res = createResponse();
+
+    (checkPassword as jest.Mock).mockResolvedValue(false);
+
+    await AuthController.login(req, res)
+    
+    const data = res._getJSONData()
+    expect(res.statusCode).toBe(401)
+    expect(data).toEqual({ error: 'Incorrect password' })
+    expect(checkPassword).toHaveBeenCalledWith(req.body.password, userMock.password)
+    expect(checkPassword).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return a JWT if auth is successful', async () => {
+
+    const userMock = {
+      id: 1,
+      email: 'tes@test.com',
+      password: 'hashed_password',
+      confirmed: true
+    };
+    
+    const req = createRequest({
+      method: 'POST',
+      url: '/api/auth/login',
+      body: {
+        email: 'test@test.com',
+        password: 'passoword'
+      }
+    })
+    const res = createResponse();
+
+    const fakejwt = 'fake_json';
+
+    (User.findOne as jest.Mock).mockResolvedValue(userMock);
+    (checkPassword as jest.Mock).mockResolvedValue(true);
+    (generateJWT as jest.Mock).mockReturnValue(fakejwt); //it is return cause is NOT async
+
+    await AuthController.login(req, res)
+    
+    const data = res._getJSONData()
+    expect(res.statusCode).toBe(200)
+    expect(data).toEqual(fakejwt)
+    expect(generateJWT).toHaveBeenCalledTimes(1)
+    expect(generateJWT).toHaveBeenCalledWith(userMock.id)
   })
 })
